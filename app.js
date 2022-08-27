9c39ccfadb12aceb2226862e75b90dd5b8465301
yassine ait laaguidi <aitlaaguidiyassine@gmail.com>
2022-08-27 13:08:02 +0200

    getData function finished

--- app.js
+++ app.js
@@ -6,10 +6,52 @@
 // .then((data) => {
 //     console.log(data);
 // })
+const api = "https://www.breakingbadapi.com/api/characters/"
 
-async function getInfos() {
-  const response = await fetch("https://www.breakingbadapi.com/api/characters");
+async function getData() {
+  const response = await fetch(api);
   const data = await response.json();
-  console.log(data);
+  printData(data);
+
+// const print = data.map(m => m.name)
+//   console.log(print);
+
+//   data.map(function(actor) {
+//        console.log(actor.name);
+//   })
+
+//   document.querySelector("#content h1").innerHTML = data[0].name;
+//   document.querySelector("#content h4").innerHTML = data[0].birthday;
+//   document.querySelector("#content img").src = data[0].img;
+
+//   document.querySelector("#actor").innerHTML = 
+//   `
+//     <select name="" id="">
+//         ${
+//             data.map(actor => `<option>${actor.name}</option>`)
+//         }
+//     </select>
+   
+//   `
+  
 }
-getInfos();
+getData();
+
+function printData(data) {
+     const header = document.querySelector("#header");
+     const content = document.querySelector("#content");
+
+    //  header.innerHTML += `
+    //                       <select class="form-control>
+    //                         <option>PPlease select</option>
+    //                         ${data.map(character => `<option>${character.name}</option>`)}
+    //                       </select>`
+    header.innerHTML += 
+    `<select class="form-control" onchange="getCharacter(this.value)>
+       <option>Please select</option>
+       ${data.map(character => `<option>${character.name}</option>`)}
+    </select>`
+                          
+}
+printData();
+
